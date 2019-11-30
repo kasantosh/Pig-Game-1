e14d1d472ba21bfd5406b9ce005cc7d9c958d3bf
Santosh Kumar <kasantosh@yahoo.com>
2019-11-30 09:55:41 -0500

    Dice functionality done

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
 
 document.querySelector('.dice').style.display = 'none';
 
